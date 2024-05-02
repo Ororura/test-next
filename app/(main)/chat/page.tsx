378dacf133dde89054efce9b01aba301abfb186e
Ororura <hitmanol890@gmail.com>
2024-05-02 22:23:27 +0300

    add chat boilerplate

---
+++ app/(main)/chat/page.tsx
@@ -0,0 +1,12 @@
+import { ChatWidget } from '@/widgets/chat-widget';
+import { FC } from 'react';
+
+const Page: FC = () => {
+  return (
+    <div>
+      <ChatWidget></ChatWidget>
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/widgets/chat-widget/ui/ChatWidget.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const ChatWidget: FC = () => {
+  return <div>Chat</div>;
+};
+
+export default ChatWidget;
